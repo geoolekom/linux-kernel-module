bd9bbc3b9992c95b63533ba0472810fb58595fe6
Yehor Komarov <geoolekom@gmail.com>
2026-02-19 15:25:37 +0400

    processes list

--- src/main.c
+++ src/main.c
@@ -1,10 +1,8 @@
-#include <linux/dcache.h>
-#include <linux/in.h>
-#include <linux/inet.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
-#include <linux/path.h>
+#include <linux/sched.h>
+#include <linux/sched/signal.h>
 
 static int log_level = 0;
 static char *module_name = "hello";
@@ -15,23 +13,27 @@ MODULE_PARM_DESC(log_level, "Log level");
 module_param(module_name, charp, 0644);
 MODULE_PARM_DESC(module_name, "Module name");
 
-static void log_file_access(struct file *filp) {
-  struct dentry *dentry = filp->f_path.dentry;
-  pr_info("File: %pd --- %pd4\n", dentry, dentry);
-}
-
 static int __init main_init(void) {
-  __be32 ipv4_addr = in_aton("192.168.1.100");
-  unsigned char mac[6] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55};
-  pr_info("IPv4: %pI4\n", &ipv4_addr);
-  pr_info("MAC: %pM\n", mac);
   pr_info("Module loaded %d %s", log_level, module_name);
-  pr_info("Current PID: %d\n", current->pid);
+  pr_info("Current init stats: %d %s %d\n", current->pid, current->comm,
+          current->tgid);
+
+  struct task_struct *task;
+  int count = 0;
+  rcu_read_lock();
+  for_each_process(task) {
+    pr_info("Process: %d %s %d\n", task->pid, task->comm, task->tgid);
+    count++;
+  }
+  rcu_read_unlock();
+  pr_info("Total processes: %d\n", count);
   return 0;
 }
 
 static void __exit main_exit(void) {
   pr_info("Module unloaded");
+  pr_info("Current exit stats: %d %s %d\n", current->pid, current->comm,
+          current->tgid);
   return;
 }
 
