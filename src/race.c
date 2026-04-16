2af4de0d57472c4426f3f20a9b8237b0bd790c9b
Yehor Komarov <ekomarov@constructor.tech>
2026-04-16 14:20:17 +0400

    fixing race ending kthread

--- src/race.c
+++ src/race.c
@@ -10,22 +10,33 @@ static DEFINE_PER_CPU(unsigned long, counter);
 static struct task_struct *task1, *task2;
 
 static int increment_function(void* data) {
-  while (!kthread_should_stop() && counter < MAX_COUNT) {
-    pr_info("Counter pid=%d: %lu", current->pid, counter);
-    this_cpu_inc(counter);
+  while (!kthread_should_stop()) {
+    if (this_cpu_read(counter) < MAX_COUNT) {
+      pr_info("Counter pid=%d: %lu", current->pid, this_cpu_read(counter));
+
+      struct sysinfo si;
+      si_meminfo(&si);
+      pr_info("monitor: total=%lu MiB free=%lu MiB\n",
+              si.totalram >> (20 - PAGE_SHIFT),
+              si.freeram >> (20 - PAGE_SHIFT));
+      this_cpu_inc(counter);
+    }
   }
   return 0;
 }
 
 static int __init race_init(void) {
   pr_info("Loading race!\n");
-  struct sysinfo si;
-  si_meminfo(&si);
-  pr_info("monitor: total=%lu MiB free=%lu MiB\n",
-          si.totalram >> (20 - PAGE_SHIFT), si.freeram >> (20 - PAGE_SHIFT));
-
   task1 = kthread_run(increment_function, NULL, "inc1");
+  if (IS_ERR(task1)) {
+    pr_err("Failed to create inc1 task: %ld\n", PTR_ERR(task1));
+    return PTR_ERR(task1);
+  }
   task2 = kthread_run(increment_function, NULL, "inc2");
+  if (IS_ERR(task2)) {
+    pr_err("Failed to create inc2 task: %ld\n", PTR_ERR(task2));
+    return PTR_ERR(task2);
+  }
   return 0;
 }
 
